20f5dd226d17a40d2033b446b631fef4d35fee6e
master king <farhad>
2022-09-19 14:38:15 +0300

    Add User Repository Service Provider

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,36 +23,36 @@ class UserController extends AppBaseController
     public function register(RegisterRequest $request){
         try {
             $registerTokenData = $this->authService->register(['name'=>$request->name,'email'=>$request->email,'password'=>$request->password]);
-            $this->response->success($registerTokenData,'success');
+            return $this->response->success($registerTokenData,'success');
         }catch (\Exception $exception){
-            $this->response->error($exception->getMessage());
+            return $this->response->error($exception->getMessage());
         }
     }
 
     public function login(LoginRequest $request){
         try {
             $authTokenData = $this->authService->login($request->only(['email','password']));
-            $this->response->success($authTokenData,'success');
+            return $this->response->success($authTokenData,'success');
         }catch (\Exception $exception){
-            $this->response->error($exception->getMessage());
+            return $this->response->error($exception->getMessage());
         }
     }
 
     public function logout(){
         try {
             $this->authService->logout();
-            $this->response->success(null,'user logout successfully');
+            return $this->response->success(null,'user logout successfully');
         }catch (\Exception $exception){
-            $this->response->error($exception->getMessage());
+            return $this->response->error($exception->getMessage());
         }
     }
 
     public function refresh(){
         try {
             $authTokenData = $this->authService->refresh();
-            $this->response->success($authTokenData,'success');
+            return $this->response->success($authTokenData,'success');
         }catch (\Exception $exception){
-            $this->response->error($exception->getMessage());
+            return $this->response->error($exception->getMessage());
         }
     }
 }
--- config/auth.php
+++ config/auth.php
@@ -14,8 +14,9 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
-        'passwords' => 'users',
+        'guard' => 'api',
+        'passwords' => 'users
+        ',
     ],
 
     /*
